1d48c49004b8128cae2e008f4d33b44bfe14d6f1
AmanYadav007 <amanry3000@gmail.com>
2024-01-20 23:22:43 +0530

    Upload v1 all files

---
+++ README.md
@@ -0,0 +1,26 @@
+
+<html>
+<body>
+   <div> DID You Know chrome extension</div>
+<p>This extion help our team to share knowledge and out managers to track the knowledge of our team</p>
+    <h1>😎😎😎😎😎😎</h1>
+
+<h1>Developers of this tool</h1>
+<p>The Cool Guy Aman & The Cool and smart & Intelegent Nikhil Bobade
+
+<h1>We have use tech like</h1>
+
+<div>
+      <p><img src="https://cdn-icons-png.flaticon.com/512/5968/5968267.png" width="30px"> HTML</p>
+      <p><img src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png" width="30px"> CSS</p>
+      <p><img src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png" width="30px"> Javascript</p>
+      <p><img src="https://cdn-icons-png.flaticon.com/512/2906/2906274.png" width="30px"> MongoDB</p>
+  </div>
+    <div id="header" align="center">
+  <img src="https://media.giphy.com/media/M9gbBd9nbDrOTu1Mqx/giphy.gif" width="150"/>
+       <img src="https://miro.medium.com/max/640/1*lpW-wLZrQ_K3XqcytkV1Cg.gif" width="150"/>
+</div>
+</body>
+</html>
+
+
--- popup.js
+++ popup.js
@@ -1,17 +1,10 @@
-// popup.js
-document.addEventListener('DOMContentLoaded', function () {
-    fetch(chrome.extension.getURL('data.json'))
-      .then(response => response.json())
-      .then(data => {
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const randomDefinition = data[randomIndex];
-  
-        const definitionElement = document.getElementById('definition');
-        const authorElement = document.getElementById('author');
-  
-        definitionElement.textContent = `"${randomDefinition.definition}"`;
-        authorElement.textContent = `– ${randomDefinition.author}`;
-      })
-      .catch(error => console.error('Error fetching quotes:', error));
-  });
-  
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('https://raw.githubusercontent.com/AmanYadav007/DUK-api/main/data.json')
+    .then(response => response.json())
+    .then(data => {
+      const randomTextElement = document.getElementById('randomText');
+      const randomIndex = Math.floor(Math.random() * data.length);
+      randomTextElement.innerText = data[randomIndex];
+    })
+    .catch(error => console.error('Error fetching random text:', error));
+});
